1b2d62e93b4829ca1c205113f0a8c54869c76db6
lonely43 <iladenisov327@gmail.com>
2025-06-07 16:28:46 +0300

    [feat] make nav + little deal with .hero

--- src/scripts/index/index.js
+++ src/scripts/index/index.js
@@ -1,1 +1,27 @@
-//
+document.querySelector("nav .burger-btn").addEventListener(
+	"click",
+	() => {
+		document.querySelector("nav .burger-btn").classList.toggle("opened")
+		document.querySelector("nav .nav-links").classList.toggle("opened-menu")
+
+	//	blackScreenAnim(document.querySelector("nav .burger-btn").classList.contains("opened"))
+	},
+	false
+)
+
+// function blackScreenAnim(state) {
+// 	let screen = document.querySelector("nav .blackScreen")
+
+// 	if (state) {
+// 		screen.style.display = "flex"
+// 		screen.style.opacity = 0
+// 		setTimeout(() => {
+// 			screen.style.opacity = 1
+// 		}, 0)
+// 	} else {
+// 		screen.style.opacity = 0
+// 		setTimeout(() => {
+// 			screen.style.display = "none"
+// 		}, 200)
+// 	}
+// }
